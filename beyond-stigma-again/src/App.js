8302d2975374772cd66ce053f243a27ca615abcd
shuchitpant <emailshuchitpant@gmail.com>
2020-07-09 18:10:09 +0530

    Made a makeshift top that will satisfy for now but will soon change

--- beyond-stigma-again/src/App.js
+++ beyond-stigma-again/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Homepage from './homepage.js'
 
 function App() {
   return (
     <div>
+      <Homepage />
       <h1>HELLO EVERYONE THIS IS WHERE WE BEGIN</h1>
       
     </div>
---
+++ beyond-stigma-again/src/homepage.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './homepage.css'
+
+function Homepage() {
+    return(
+        <div>
+                <div class="topbar">
+                    <div class="topbar-elements">
+                       <a href="" class="topbar-elements-individuals"> Home</a>
+                    </div>
+                    <div class="topbar-elements">
+                        <a href="" class="topbar-elements-individuals"> Home </a>
+                    </div>
+                    <div  class="topbar-elements">
+                        <a href="" class="topbar-elements-individuals">Home </a> 
+                    </div>
+                    <div class="topbar-elements">
+                        <a href="" class="topbar-elements-individuals"> Home </a>
+                    </div>
+
+
+                </div>
+                <h1>home    </h1>
+                <h1>home    </h1>    
+                <h1>home    </h1>
+                <h1>home    </h1>
+            
+        </div>
+        
+    );
+}
+
+export default Homepage;
